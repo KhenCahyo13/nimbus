813564d7ed80a6f920ae457d362caa746c5e944f
KhenCahyo13 <khencahyo02@gmail.com>
2026-03-13 13:37:39 +0700

    refactor(environment): change function name

--- resources/js/components/common/KeyValueParameters/KeyValueParameters.vue
+++ resources/js/components/common/KeyValueParameters/KeyValueParameters.vue
@@ -36,7 +36,7 @@ export interface AppKeyValueParametersProps {
     modelValue?: ParameterContract[];
     freeFormTypes?: boolean;
     class?: HTMLAttributes['class'];
-    getValueInputClass?: (parameter: ParameterContract) => HTMLAttributes['class'];
+    getValueInputExtraClassUsing?: (parameter: ParameterContract) => HTMLAttributes['class'];
 }
 
 export interface AppKeyValueParametersEmits {
@@ -224,7 +224,7 @@ const handleDeleteParameter = (index: number) => {
                     <AppInput
                         v-model="parameter.value"
                         placeholder="Value"
-                        :class="cn('pl-panel h-full flex-1 rounded-none border-0 border-r shadow-none focus:ring-0 focus-visible:ring-0', props.getValueInputClass?.(parameter))"
+                        :class="cn('pl-panel h-full flex-1 rounded-none border-0 border-r shadow-none focus:ring-0 focus-visible:ring-0', props.getValueInputExtraClassUsing?.(parameter))"
                         :disabled="!parameter.enabled"
                         name="kv-value"
                         data-testid="kv-value"
